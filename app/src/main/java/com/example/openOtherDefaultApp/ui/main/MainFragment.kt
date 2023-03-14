74f186851ebc1fe10f6d0bdead19584be3e831f7
compadre <zvvlad2@gmail.com>
2023-03-14 17:13:39 +0600

    VE: first commit

---
+++ app/src/main/java/com/example/openOtherDefaultApp/ui/main/MainFragment.kt
@@ -0,0 +1,39 @@
+package com.example.openOtherDefaultApp.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.openOtherDefaultApp.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return FragmentMainBinding.inflate(inflater).also {
+            binding = it
+        }.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        // TODO
+    }
+}
