f0d0d1216d5cbbde33afe44a235894501a00b7d8
compadre <zvvlad2@gmail.com>
2023-03-15 18:35:19 +0600

    VE: add Url open and Text send

--- app/src/main/java/com/example/openOtherDefaultApp/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/openOtherDefaultApp/ui/main/MainFragment.kt
@@ -1,9 +1,16 @@
 package com.example.openOtherDefaultApp.ui.main
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.pm.PackageManager.MATCH_ALL
+import android.content.pm.PackageManager.MATCH_DEFAULT_ONLY
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.openOtherDefaultApp.databinding.FragmentMainBinding
@@ -19,8 +26,7 @@ class MainFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
     }
 
     override fun onCreateView(
@@ -34,6 +40,34 @@ class MainFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        // TODO
+        binding.apply {
+            urlOpenButton.setOnClickListener {
+                val webpage = Uri.parse(MainViewModel.DEFAULT_URL)
+                val intent = Intent(Intent.ACTION_VIEW, webpage)
+
+                val chooser = Intent.createChooser(intent, null)
+                startActivity(chooser)
+
+//              Или можно так, в данном случае, насколько я понимаю, откроет первое попавшееся
+//              if (intent.resolveActivity(requireContext().packageManager) != null) {
+//                  startActivity(intent)
+//              }
+            }
+            textSendButton.setOnClickListener {
+                val sendIntent: Intent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    putExtra(Intent.EXTRA_TEXT, "This is my text to send.")
+                    type = "text/plain"
+                }
+
+                val shareIntent = Intent.createChooser(sendIntent, null)
+                startActivity(shareIntent)
+
+//              Или можно так, в данном случае использует какой-то дефолтный Chooser
+//              if (sendIntent.resolveActivity(requireContext().packageManager) != null) {
+//                    startActivity(sendIntent)
+//              }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/openOtherDefaultApp/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/openOtherDefaultApp/ui/main/MainViewModel.kt
@@ -3,5 +3,8 @@ package com.example.openOtherDefaultApp.ui.main
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    companion object {
+        const val DEFAULT_URL = "https://ru.wikipedia.org/wiki/Hello,_world!"
+    }
 }
